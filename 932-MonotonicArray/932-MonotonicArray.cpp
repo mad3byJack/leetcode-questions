a4eca3d6b8337686d7e5ea8a58899525b6b1564d
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-08-30 10:07:43 +1000

    Create 932-MonotonicArray

---
+++ 932-MonotonicArray/932-MonotonicArray.cpp
@@ -0,0 +1,27 @@
+// Last updated: 30/08/2026, 10:07:43 am
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        if (nums.size() == 1 || nums.size() == 2) {
+            return true;
+        }
+        int dir = 0;
+        int last = nums[0];
+        for (int i = 1; i < nums.size(); i ++) {
+            if (nums[i] > last) {
+                if (dir == -1) {
+                    return false;
+                }
+                dir = 1;
+            }
+            if (nums[i] < last) {
+                if (dir == 1) {
+                    return false;
+                }
+                dir = -1;
+            }
+            last = nums[i];
+        }
+        return true;
+    }
+};
