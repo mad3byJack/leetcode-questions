ecd853e6341acfa831c745b6eea0579f0d91ad3e
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-02 17:36:21 +1000

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 02/09/2026, 5:36:21 pm
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        std::unordered_map<int, int> elements;
+        std::vector<int> ans;
+        int min = nums.size() / 3;
+        for (int i = 0; i < nums.size(); i++) {
+            elements[nums[i]] ++;
+        }
+        for (const auto& [key, value] : elements) {
+            if (value > min) {
+                ans.push_back(key);
+            }
+        }
+        return ans;
+    }
+};
