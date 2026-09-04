b27353d18e781f5f75eaa074693a4ca3a5304bd2
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-04 11:18:17 +1000

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.cpp
@@ -0,0 +1,19 @@
+// Last updated: 04/09/2026, 11:18:16 am
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.size() > t.size()) {
+            return false;
+        }
+        int i = 0;
+        for (int x = 0; x < t.size(); x++) {
+            if (t[x] == s[i]) {
+                i++;
+            }
+        }
+        if (i == s.size()) {
+            return true;
+        }
+        return false;
+    }
+};
