d4cc3b35f7a8acff584787fffebe8fdc4304036b
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-05 11:40:09 +1000

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -0,0 +1,22 @@
+// Last updated: 05/09/2026, 11:40:08 am
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            int x = 0;
+            while (nums1[i] != nums2[x]) {
+                x ++;
+            }
+            int greatest = -1;
+            for (int j = x; j < nums2.size(); j++) {
+                if (nums2[j] > nums1[i]) {
+                    greatest = nums2[j];
+                    break;
+                }
+            }
+            ans.push_back(greatest);
+        }
+        return ans;
+    }
+};
