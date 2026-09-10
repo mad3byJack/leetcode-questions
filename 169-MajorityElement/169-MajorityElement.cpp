30ce220fefc194324df81c8ae8cb5ccf2a894e44
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-10 13:35:24 +1000

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,17 @@
+// Last updated: 10/09/2026, 1:35:24 pm
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int half = nums.size() / 2;
+        unordered_map<int, int> table;
+        for (int i : nums) {
+            table[i] ++;
+        }
+        for (const auto& [num, freq] : table) {
+            if (freq > half) {
+                return num;
+            }
+        }
+        return 0;
+    }
+};
