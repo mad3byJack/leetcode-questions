29f94b60870604e9bf2535892580839aaac67461
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-18 10:16:32 +1000

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
@@ -0,0 +1,38 @@
+// Last updated: 18/09/2026, 10:16:33 am
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<int> stk;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+") {
+                int temp = 0;
+                temp += stk.top();
+                stk.pop();
+                temp += stk.top();
+                stk.pop();
+                stk.push(temp);
+            } else if (tokens[i] == "-") {
+                int temp = stk.top();
+                stk.pop();
+                temp = stk.top() - temp;
+                stk.pop();
+                stk.push(temp);
+            } else if (tokens[i] == "*") {
+                int temp = stk.top();
+                stk.pop();
+                temp *= stk.top();
+                stk.pop();
+                stk.push(temp);
+            } else if (tokens[i] == "/") {
+                int temp = stk.top();
+                stk.pop();
+                temp = stk.top() / temp;
+                stk.pop();
+                stk.push(temp);
+            } else {
+                stk.push(stoi(tokens[i]));
+            }
+        }
+        return stk.top();
+    }
+};
