e940cb4cedbdce588e70a76edf53e08a286016c7
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-09-21 10:41:02 +1000

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+// Last updated: 21/09/2026, 10:41:02 am
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n = heights.size();
+        int left = 0;
+        int right = n - 1;
+        int max_water = 0;
+        while (right > left) {
+            int water = std::min(heights[left], heights[right]) * (right - left);
+            if (water > max_water) {
+                max_water = water;
+            }
+            if (heights[left] > heights[right]) {
+                right --;
+            } else {
+                left ++;
+            }
+        }
+        return max_water;
+    }
+};
